8b3befce1f988b5053a6e61f947da3323a74a990
Terminator1140 <yaswanthmunagala2001@gmail.com>
2025-09-29 15:46:56 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Simple Interest Calculator
 
+if [ $# -ne 3 ]; then
+  echo "Usage: bash $0 PRINCIPAL RATE TIME"
+  exit 1
+fi
+
+PRINCIPAL=$1
+RATE=$2
+TIME=$3
+
+INTEREST=$(echo "scale=2; $PRINCIPAL * $RATE * $TIME / 100" | bc)
+echo "Simple Interest is: $INTEREST"
